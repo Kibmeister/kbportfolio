688d2fda2fcd33c58131214980c583d71b11b25f
Kasper Iverslien Borgbjerg <201808262@post.au.dk>
2023-08-05 12:27:48 +0200

    page scroll and header link clicks behavior needs care

--- src/utils/useIntersectionObserver.js
+++ src/utils/useIntersectionObserver.js
@@ -4,32 +4,31 @@ import { useEffect, useState } from 'react';
 export const useIntersectionObserver = (elements, options) => {
   const [entries, setEntries] = useState([]);
 
-   useEffect(() => {
-     const observer = new IntersectionObserver((entries) => {
-       // Find the entry with the highest intersection ratio.
-       const activeEntry = entries.reduce(
-         (highest, entry) => {
-           return entry.intersectionRatio > highest.intersectionRatio
-             ? entry
-             : highest;
-         },
-         { intersectionRatio: 0 }
-       );
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      // Find the entry with the highest intersection ratio.
+      const activeEntry = entries.reduce(
+        (highest, entry) => {
+          return entry.intersectionRatio > highest.intersectionRatio
+            ? entry
+            : highest;
+        },
+        { intersectionRatio: 0 }
+      );
 
-       setEntries([activeEntry]);
-     }, options);
+      setEntries([activeEntry]);
+    }, options);
 
-     elements.forEach((element) => {
-       if (element) observer.observe(element);
-     });
+    elements.forEach((element) => {
+      if (element) observer.observe(element);
+    });
 
-     return () => {
-       elements.forEach((element) => {
-         if (element) observer.unobserve(element);
-       });
-     };
-   }, [elements, options]);
-
+    return () => {
+      elements.forEach((element) => {
+        if (element) observer.unobserve(element);
+      });
+    };
+  }, [elements, options]);
 
   return entries;
 };
