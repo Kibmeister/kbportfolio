dad21c4ce15fbb77e43692405b6cb12d536787ca
Kasper Iverslien Borgbjerg <201808262@post.au.dk>
2023-08-09 15:41:53 +0200

    still debugging

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -52,13 +52,14 @@ const ProjectCard = ({
 
           <div className='absolute  inset-0 flex justify-end m-3 card-img-hover'></div>
         </div>
-        {/*
+
         <div className='mt-5 '>
           <h3 className='garet-book font-bold text-[24px]'>{name}</h3>
           <p className='garet-book mt-2 text-black text-[14px]'>
             {description}
           </p>
         </div>
+        {/*
         <div className='mt-4 flex flex-wrap gap-2'>
           {tags.map((tag) => (
             <p key={tag.name} className={`text[14px-] ${tag.color}`}>
