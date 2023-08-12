826878e30641eec3a282c5c376ff0c043cd79c10
Kasper Iverslien Borgbjerg <201808262@post.au.dk>
2023-08-12 14:00:49 +0200

    hero section responsive 1.1

--- src/styles.js
+++ src/styles.js
@@ -9,17 +9,17 @@ const styles = {
   heroHeadText:
     'hero-head-text 2xl:text-[100px] xl:text-[90px] lg:text-[80px] md:text-[70px] sm:text-[60px] mobile:text-[50px]  text-[40px]  mt-2 break-words break-normal',
   heroSubText:
-    'hero-sub-text  2xl:text-[20px] xl:text-[18px] lg:text-[16px] md:text-[14px] sm:text-[12px] mobile:text-[10px] text-[16px] break-words break-normal',
+    'hero-sub-text  2xl:text-[20px] xl:text-[18px] lg:text-[16px] md:text-[14px] sm:text-[14px] mobile:text-[14px] text-[16px] break-words break-normal',
   sectionHeadText:
     'section-head-text md:text-[60px] sm:text-[50px] mobile:text-[40px] text-[30px] break-words break-normal',
   sectionSubText:
     'section-sub-text sm:text-[18px] text-[14px] uppercase tracking-wider break-words break-normal',
   menubrandText:
-    '2xl:text-[18px] xl:text-[16px] lg:text-[14px] md:text-[12px] sm:text-[12px] mobile:text-[10px] font-garet-heavy cursor-pointer flex',
+    '2xl:text-[18px] xl:text-[16px] lg:text-[14px] md:text-[14px] sm:text-[14px] mobile:text-[14px] font-garet-heavy cursor-pointer flex',
   menulinkText:
-    '2xl:text-[18px] xl:text-[16px] xl:text-[14px] md:text-[12px] garet-book font-medium cursor-pointer',
+    '2xl:text-[18px] xl:text-[16px] xl:text-[14px] md:text-[14px] garet-book font-medium cursor-pointer',
   menulinkTextHamburger:
-    'sm:text-[14px] mobile:text-[12px] garet-book font-medium cursor-pointer    ',
+    'sm:text-[14px] mobile:text-[14px] garet-book font-medium cursor-pointer ',
 };
 
 export { styles };
